9cef88c5a50fda35f603683d34d5c3287980a3b8
adeeb <chilipadiboy@hotmail.com>
2018-03-03 19:23:19 +0800

    create project sql and form. no page creation yet.

--- createproject.php
+++ createproject.php
@@ -69,6 +69,10 @@ if ($result = $conn->query($sql_select_user_id)) {
 						<td><input type="text" name="category"></td>
 					</tr>
 					<tr>
+						<td>Funding Goal</td>
+						<td><input type="number" name="goal"></td>
+					</tr>
+					<tr>
 						<td>Keyword</td>
 						<td><input type="text" name="keyword"></td>
 					</tr>
@@ -78,25 +82,47 @@ if ($result = $conn->query($sql_select_user_id)) {
                 </table>
             </form>
         </fieldset>';	
-		
+?>
+
+<?php
 $error_message="";
 if(isset($_POST['createproj'])){
 	$title= mysqli_real_escape_string($conn, $_POST['title']);
 	$imgurl=mysqli_real_escape_string($conn, $_POST['image']);
 	$description=mysqli_real_escape_string($conn, $_POST['description']);
-	$enddate=strtotime($_POST["enddate"]);
+	$enddate=$_POST["enddate"];
 	$category=mysqli_real_escape_string($conn, $_POST['category']);
-	$keyword=mysqli_real_escape_string($conn, $_POST['keyword']);
+	$goal=$_POST['goal'];
+	$keyword=trim(mysqli_real_escape_string($conn, $_POST['keyword']));
+	$email=$_SESSION['email'];
 	
 	//get user_id
-	$sql_select_userid = "SELECT user_id FROM users WHERE email='$_SESSION['email']'";
-	
+	$sql_select_userid = "SELECT user_id FROM users WHERE email='$email'";
+	if($result= $conn->query($sql_select_userid)){
+		$row=$result->fetch_assoc();
+		$user_id=$row['user_id'];
+		//create category no check
+		$sql_create_category="Insert into categories (category) values ('$category')";
+		$conn->query($sql_create_category);
+		//create project
+		$sql_create_project="Insert into projects (project_id, user_id, title, image_url, description,end_datetime,category,funding_goal)
+								values (NULL, '$user_id', '$title', '$imgurl','$description','$enddate','$category','$goal')";
+		if($conn->query($sql_create_project)){
+			$error_message=$conn->query($sql_create_project);
+			$error_message="Successfully created project";
+		}else{
+			$error_message=$conn->error;
+		}
+		
+	}else{
+		$error_message="Failed to get user id. Please check if you are logged in";
+	}
 	
 }else{
-	echo 'Fill in all the fields';
+	$error_message="Please fill in all the fields";
 }
 
-
+echo $error_message;
 ?>
 	</body>
 </html>
