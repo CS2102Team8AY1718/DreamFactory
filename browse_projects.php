915ca2228e1bbb1cbd50fbaa6ea3a45e09247636
AFICAT\Afiqah Rashid <aficatrashid@gmail.com>
2018-04-10 16:14:24 +0800

    added only the home tab

--- browse_projects.php
+++ browse_projects.php
@@ -245,14 +245,16 @@ if ($result = $conn->query($sql_select_unfunded_projects)) {
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color:Black;" id="mainNav">
       <div class="container">
-          <a class="navbar-brand js-scroll-trigger" href="homepage.php">DreamFactory</a>
+          <a class="navbar-brand js-scroll-trigger" href="#page-top">DreamFactory</a>
           <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
               Menu
               <i class="fa fa-bars"></i>
           </button>
           <div class="collapse navbar-collapse" id="navbarResponsive">
               <ul class="navbar-nav text-uppercase ml-auto">
-
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="homepage.php">Home</a>
+                </li>
                   <li class="nav-item">
                       <a class="nav-link js-scroll-trigger" href="#ongoingProjects">Ongoing Projects</a>
                   </li>
